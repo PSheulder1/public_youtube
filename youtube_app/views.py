1ea5bba84e33229f48b9ea5470333c0dbca094bb
PSheulder1 <psheulder@gmail.com>
2025-06-30 11:48:24 -0700

    Update views.py

--- youtube_app/views.py
+++ youtube_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from authentification_app.models import Profile
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    try:
+        profile = Profile.objects.get(user=request.user)
+    except Exception as e:
+        profile = None
+    return render(request, 'index.html', {'profile':profile})
