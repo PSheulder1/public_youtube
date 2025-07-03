d8d1ea8adf5e37ef04ef87adb304b4e665df230d
PSheulder1 <psheulder@gmail.com>
2025-07-03 13:17:55 -0700

    Update models.py

--- youtube_app/models.py
+++ youtube_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class YoutubeChannel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='channel_image/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
