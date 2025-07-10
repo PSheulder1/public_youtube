b402df98ebb016c78835e9c6b10c62ab763ab01b
PSheulder1 <psheulder@gmail.com>
2025-07-10 13:11:03 -0700

    Update urls.py

--- youtube_app/urls.py
+++ youtube_app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import home, channel, chaine_details
+from .views import home, channel, chaine_details, upload
 
 
 urlpatterns =  [
     path('', home, name='home'),
     path('create/channel/', channel, name='channel'),
     
-    path('chaine/details/<int:pk>', chaine_details, name='chaine')
+    path('chaine/details/<int:pk>', chaine_details, name='chaine'),
+
+    path ('upload/<int:pk>', upload, name='upload')
 
 ]
 
