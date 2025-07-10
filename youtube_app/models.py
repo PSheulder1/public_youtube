17569f59280b4bcd54d927ee0d41c58ac1d0c98f
PSheulder1 <psheulder@gmail.com>
2025-07-10 12:24:01 -0700

    Update models.py

--- youtube_app/models.py
+++ youtube_app/models.py
@@ -10,3 +10,14 @@ class YoutubeChannel(models.Model):
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='channel_image/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Video(models.Model):
+    titre = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    video = models.FileField(upload_to='videos/', blank=True,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    channel = models.ForeignKey(YoutubeChannel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
